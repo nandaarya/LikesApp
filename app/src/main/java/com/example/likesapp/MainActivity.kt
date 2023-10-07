2e7fcd99dc124708689cc9ef9d04bdf5212c1232
Nanda Arya Putra <nandaarya404@gmail.com>
2023-10-07 16:39:44 +0700

    add clipping

--- app/src/main/java/com/example/likesapp/MainActivity.kt
+++ app/src/main/java/com/example/likesapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.likesapp
 
 import android.graphics.*
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.res.ResourcesCompat
@@ -31,15 +32,21 @@ class MainActivity : AppCompatActivity() {
         binding.imageView.setImageBitmap(mBitmap)
 
         binding.like.setOnClickListener {
+            showEars()
             showFace()
             showMouth(true)
             showEyes()
+            showNose()
+            showHair()
         }
 
         binding.dislike.setOnClickListener {
+            showEars()
             showFace()
             showMouth(false)
             showEyes()
+            showNose()
+            showHair()
         }
 
         showText()
@@ -118,5 +125,51 @@ class MainActivity : AppCompatActivity() {
         val x: Float = halfOfWidth - mBounds.centerX()
         val y = 50F
         mCanvas.drawText(message, x, y, mPaintText)
+    }
+
+    private fun showNose() {
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+        mCanvas.drawCircle(halfOfWidth - 40F, halfOfHeight + 140F, 15F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 40F, halfOfHeight + 140F, 15F, mPaint)
+    }
+
+    private fun showEars() {
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_left_hair, null)
+        mCanvas.drawCircle(halfOfWidth - 300F, halfOfHeight - 100F, 100F, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_right_hair, null)
+        mCanvas.drawCircle(halfOfWidth + 300F, halfOfHeight - 100F, 100F, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.red_ear, null)
+        mCanvas.drawCircle(halfOfWidth - 300F, halfOfHeight - 100F, 60F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 300F, halfOfHeight - 100F, 60F, mPaint)
+    }
+
+    private fun showHair() {
+        mCanvas.save()
+
+        val path = Path()
+
+        path.addCircle(halfOfWidth - 100F,halfOfHeight - 10F, 150F, Path.Direction.CCW)
+        path.addCircle(halfOfWidth + 100F,halfOfHeight - 10F, 150F, Path.Direction.CCW)
+
+        val mouth = RectF(halfOfWidth - 250F, halfOfHeight, halfOfWidth + 250F, halfOfHeight + 500F)
+        path.addOval(mouth, Path.Direction.CCW)
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            mCanvas.clipPath(path, Region.Op.DIFFERENCE)
+        } else {
+            mCanvas.clipOutPath(path)
+        }
+
+        val face = RectF(left, top, right, bottom)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_left_hair, null)
+        mCanvas.drawArc(face, 90F, 180F, false, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_right_hair, null)
+        mCanvas.drawArc(face, 270F, 180F, false, mPaint)
+
+        mCanvas.restore()
     }
 }
